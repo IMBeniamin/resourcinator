a87ff500cac38dd663e756c3388ef75af764f757
IM Beniamin <beniiorga@gmail.com>
2024-10-23 00:40:39 +0200

    added favicon added components folder to structure setup initial components templates saved launch configs to files

---
+++ src/components/Resource.svelte
@@ -0,0 +1,29 @@
+<script>
+	export let title = "Default Title";
+	export let icon;
+	export let open = false;
+
+	function handleClick() {
+		open = !open;
+	}
+</script>
+
+<div>
+	<!-- Slot allows you to pass HTML content to the component -->
+	<slot></slot>
+
+	<h2>{title}</h2>
+	<h3>isopen: {open}</h3>
+	<button on:click={handleClick}>Click Me</button>
+</div>
+
+<style>
+    h2 {
+        color: green;
+    }
+    button {
+        background-color: orange;
+        border: none;
+        padding: 10px;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Resource from '../components/Resource.svelte';
+</script>
+
+<style>
+    :global(body) {
+        background-color: #121212;
+        color: #e0e0e0;
+        font-family: Arial, sans-serif;
+    }
+</style>
+
+<div>
+	<Resource title="Group 1" icon="🔥">
+	</Resource>
+</div>
